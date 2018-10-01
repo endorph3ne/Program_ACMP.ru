2e960a62d9657763c622f0ddd8fcc71c118c257c
endorph3ne <endorph3ne@gmail.com>
2018-10-02 01:02:57 +0300

    Манхэттенске улицы (0817)

--- Две окружности (0026)/Program.cs
+++ Две окружности (0026)/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Две_окружности__0026_ {
     class Program {
---
+++ Манхэттенские улицы (0817)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Манхэттенские_улицы__0817_ {
+    class Program {
+        static void Main(string[] args) {
+            string[] inputStringArray = Console.ReadLine().Split(' ');
+            int n = Convert.ToInt32(inputStringArray[0]);
+            int m = Convert.ToInt32(inputStringArray[1]);
+            int d = Convert.ToInt32(inputStringArray[2]);
+            int k = Convert.ToInt32(inputStringArray[3]);
+
+            int result = (n + m) * (d * k) - n * m * d * d;
+            Console.WriteLine("Answer: {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
