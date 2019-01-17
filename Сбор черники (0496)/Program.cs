d1d409da493e7b189db6c7c2d1add09996bb896b
endorph3ne <endorph3ne@gmail.com>
2019-01-18 00:16:05 +0300

    Сбор черники (0496)

---
+++ Сбор черники (0496)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сбор_черники__0496_ {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = { 1, 2, 3, 4 };
+            int maxBerries = 0;
+            int currentBerries = 0;
+            
+
+            if (array.Length == 3)
+                Console.WriteLine($"Answer: {array[0] + array[1] + array[2]}");
+            else {
+                int numberBerriesFirstBush = array[0] + array[1] + array[array.Length - 1];
+                int numberBerriesLastBush = array[array.Length - 2] + array[array.Length - 1] + array[0];
+
+                maxBerries = numberBerriesFirstBush > numberBerriesLastBush ? numberBerriesFirstBush : numberBerriesLastBush;
+
+                for (int i = 1; i < array.Length - 1; i++) {
+                    currentBerries = 0;
+                    currentBerries = array[i - 1] + array[i] + array[i + 1];
+
+                    if (currentBerries > maxBerries)
+                        maxBerries = currentBerries;
+                }
+                Console.WriteLine($"Answer: {maxBerries}");
+            }
+
+            
+            Console.Read();
+        }
+    }
+}
