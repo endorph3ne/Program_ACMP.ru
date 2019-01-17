9de1598c1165c14f1640f7de3c596c64ad908941
endorph3ne <endorph3ne@gmail.com>
2019-01-17 23:59:47 +0300

    Оттепель (0264)

---
+++ Оттепель (0264)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Оттепель__0264_ {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = { 10, 20, 30, 1, -10, 1, 2, 3 };
+            int countDaysWithTMoreThan0 = 0;
+            int max = 0;
+
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] > 0)
+                    countDaysWithTMoreThan0++;
+                else {
+                    if (countDaysWithTMoreThan0 > max)
+                        max = countDaysWithTMoreThan0;
+                    countDaysWithTMoreThan0 = 0;
+                }
+
+            }
+            Console.WriteLine($"Answer: {max}");
+            Console.Read();
+        }
+
+    }
+}
---
+++ Оттепель (0264)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Оттепель (0264)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Оттепель (0264)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed6face3-d64c-44bb-a361-6699ce898776")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
