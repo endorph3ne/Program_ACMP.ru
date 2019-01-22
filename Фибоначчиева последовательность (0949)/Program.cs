967ebb18b5852680071632addf63086e32922c28
endorph3ne <endorph3ne@gmail.com>
2019-01-22 23:24:22 +0300

    Фибоначчиева последовательность (0949)

---
+++ Фибоначчиева последовательность (0949)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фибоначчиева_последовательность__0949_ {
+    class Program {
+        static void Main(string[] args) {
+            string[] inputStringArray = Console.ReadLine().Split(' ');
+            int n = Int32.Parse(inputStringArray[0]);
+            int aCurrent = Int32.Parse(inputStringArray[1]);
+            int aNext = Int32.Parse(inputStringArray[2]);
+
+            var result = FindFirstElimentsInSequence(n, aCurrent, aNext);
+            Console.WriteLine($"Answer: {result.first} {result.second}");
+            Console.Read();
+        }
+
+        static (int first, int second) FindFirstElimentsInSequence(int n, int aCurrent, int aNext) {
+            if (n == 1) {
+                var result = (aCurrent, aNext);
+                return result;
+            }
+
+            return FindFirstElimentsInSequence(n - 1, aNext - aCurrent, aCurrent);
+        }
+    }
+}
---
+++ Фибоначчиева последовательность (0949)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Фибоначчиева последовательность (0949)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Фибоначчиева последовательность (0949)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("529786d6-984f-41dd-918b-90323d15a14d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
