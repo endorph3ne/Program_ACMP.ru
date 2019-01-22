240d98d155e0f7b8e42aa1a4d1a5f0d9ac384df9
endorph3ne <endorph3ne@gmail.com>
2019-01-23 00:10:47 +0300

    Дороги (0015)

---
+++ Дороги (0015)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Дороги__0015_ {
+    class Program {
+        static void Main(string[] args) {
+            int[,] array = { {0, 1, 0, 0, 0 },
+                             {1, 0, 1, 1, 0 },
+                             {0, 1, 0, 0, 1 },
+                             {0, 1, 0, 0, 0 },
+                             {0, 0, 1, 0, 0 } };
+
+            int sumAllRoad = 0;
+            for (int i = 0; i < Math.Sqrt(array.Length); i++) {
+                for (int j = 0; j < Math.Sqrt(array.Length); j++) {
+                    if (array[i, j] == 1)
+                        sumAllRoad++;
+                }
+            }
+
+            Console.WriteLine($"Answer: {sumAllRoad / 2}");
+            Console.Read();
+        }
+    }
+}
