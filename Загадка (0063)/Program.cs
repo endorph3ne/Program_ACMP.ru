067cfe54bfb1ab6e7de9ec1437c291add11bb097
endorph3ne <endorph3ne@gmail.com>
2019-01-28 23:59:25 +0300

    Загадка (0063)

---
+++ Загадка (0063)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Загадка__0063_ {
+    class Program {
+        static void Main(string[] args) {
+            int P = 6;
+            int S = 5;
+
+            double D = S * S - 4 * P;
+            int x1 = (int)((S - Math.Sqrt(D)) / 2);
+            int x2 = S - x1;
+
+            Console.WriteLine($"Answer: {x1} {x2}");
+            Console.Read();
+        }
+    }
+}
