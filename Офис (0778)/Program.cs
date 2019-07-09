da99389106a9826121596fe3d5965d208146f180
endorph3ne <endorph3ne@gmail.com>
2019-07-10 00:02:13 +0300

    Офис (0778)

---
+++ Офис (0778)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Офис__0778_ {
+    class Program {
+        static void Main(string[] args) {
+            int[] array = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0 };
+            int countDaysOff = 4;
+
+            int numberOfEmployees = array.Sum();
+            int countWorkingDays = array.Length - countDaysOff;
+
+            Console.WriteLine("Answer: {0}", numberOfEmployees / countWorkingDays);
+            Console.ReadLine();
+        }
+    }
+}
